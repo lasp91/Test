8b5c28d472d413daa64212932f368f98d957e1c0
Luiz Peyon <la.de.peyon@xtra.co.nz>
2016-10-08 18:41:05 +1300

    Initial commit.

--- Program.fs
+++ Program.fs
@@ -1,7 +1,14 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+  // A very simple program for the purpose of testing the VS2015 installation.
+  let today = DateTime.Now 
+  printfn "\n\nDate and time now is: %A" today 
+  printfn "Press any key to terminate this program..."
+  Console.ReadKey(true) |> ignore 
+
+  0 // return an integer exit code
